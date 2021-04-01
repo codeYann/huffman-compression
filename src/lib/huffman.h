4d1af0b8b687fe8a88abca4b069d27d6f4630dc3
codeYann <codeyan10@gmail.com>
2021-04-01 10:25:44 -0300

    Declaração de todos os métodos que vão ser utilizados no projeto

--- src/lib/huffman.h
+++ src/lib/huffman.h
@@ -1,7 +1,12 @@
 #ifndef HUFFMAN_H
 #define HUFFMAN_H
 #include <stdio.h>
+#include "fila.h"
+
 
 // Função para importar a string de um arquivo txt
 char* importarString(char* path);
+Fila* filaFrequencia(char* texto);
 #endif
+
+
