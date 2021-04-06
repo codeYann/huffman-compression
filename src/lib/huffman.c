15e95f14d5c7d6eda1c94af6bea8154b63e6cfc3
codeYann <codeyan10@gmail.com>
2021-04-06 20:53:28 -0300

    huffman.c é o arquivo que vai conter toda a implementação dos métodos contidos no huffman.h

--- src/lib/huffman.c
+++ src/lib/huffman.c
@@ -1,55 +1,53 @@
 #include "./huffman.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /* Implementação da função para importar a string */
-char* importarString(char* path) {
-  char* string;
-  FILE* buffer = fopen(path, "r"); 
+char *importarString(char *path) {
+  char *string;
+  FILE *buffer = fopen(path, "r");
 
-  if(buffer == NULL) {
+  if (buffer == NULL) {
     printf("Erro ao tentar ler um arquivo de texto\n");
     return NULL;
   }
-  
+
   /* Calculando o tamanho, em caracteres, do meu texto */
   fseek(buffer, 0, SEEK_END);
   int size = ftell(buffer);
-  string = malloc((sizeof(char) * size + 1)); 
+  string = malloc((sizeof(char) * size + 1));
 
   fseek(buffer, 0, SEEK_SET);
   int i = 0;
   do {
     char c = fgetc(buffer);
-    if(feof(buffer))
+    if (feof(buffer))
       break;
     string[i] = c;
     i++;
-  } while(1);
+  } while (1);
 
   fclose(buffer);
-  return string; 
+  return string;
 }
 
-Fila* filaFrequencia(char* texto) {
+Lista *computarFrequencia(char *texto) {
   int count = 0;
-  Fila* fila = criarFila();
+  Lista *lista = criarLista();
 
-  for(int i = 0; texto[i] != '\0'; i++) {
+  for (int i = 0; texto[i] != '\0'; i++) {
     int j = 0;
-    while(j < i && texto[i] != texto[j])
+    while (j < i && texto[i] != texto[j])
       j++;
-    if(j==i) {
+    if (j == i) {
       count = 1;
-      for(int t = i+1; texto[t] != '\0'; t++) {
-        if(texto[i] == texto[t])
+      for (int t = i + 1; texto[t] != '\0'; t++) {
+        if (texto[i] == texto[t])
           count += 1;
-      } 
-      append(fila, texto[i],  count);
+      }
+      push(lista, texto[i], count);
     }
   }
-  percorrer(fila);
-  printf("Tamanho => %d\n", TamanhoFila(fila));
-  return fila;
+  bubbleSort(lista->inicio);
+  return lista;
 }
